907e8d54a114c237b59829ea1cd69953e57080b0
zamanvi <90791119+zamanvi@users.noreply.github.com>
2026-06-18 19:01:54 +0600

    revert: undo hardcoded [] in formBuilder init

--- backend/resources/views/filament/forms/components/form-builder.blade.php
+++ backend/resources/views/filament/forms/components/form-builder.blade.php
@@ -4,7 +4,7 @@
 @endphp
 
 <div
-    x-data="formBuilder([])"
+    x-data="formBuilder(@js(json_decode($state, true) ?: []))"
     x-init="init()"
     wire:ignore
     class="space-y-3"
@@ -215,7 +215,7 @@
     {{-- Add new field --}}
     <button type="button" @click="addGroup()"
         class="w-full border-2 border-dashed border-gray-300 rounded-xl py-3 text-sm font-medium text-gray-500 hover:bg-gray-50 hover:border-primary-300 hover:text-primary-600 transition-colors">
-        + Field Title
+        + Add Data and Document
     </button>
 </div>
 
